253d59972b480a3c4061a83cd0e9021adc6800af
AndreyUrazaev <f.a.z.a@bk.ru>
2024-08-27 11:50:00 +0500

    lesson-01-universal-button

---
+++ src/components/button.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+type ButtonPropsType = {
+  name: string;
+  callBack: () => void;
+};
+export const Button = (props: ButtonPropsType) => {
+  const onClickHandler = () => {
+    props.callBack();
+  };
+  return <button onClick={onClickHandler}>{props.name}</button>;
+};
